b2e4ebd3de538452cec41bb4c6095c4e2a3dae8c
mugasin01 <mugasin9080183671@gmail.com>
2026-09-10 13:17:27 +0530

    fetch api

--- src/Courselist.jsx
+++ src/Courselist.jsx
@@ -1,49 +1,19 @@
 import Course from './Course';
-import html from './assets/html.jpg'
-import css  from './assets/css.avif'
-import js from './assets/js.png'
-import react from './assets/react.png'
 import { useState, useEffect } from 'react';
 
 function CourseList(){
 
-    const [courses, setcourse] = useState([
-        {
-            id :1,
-            name :"HTML full Course" ,
-            price :199 ,
-            image : html ,
-            rating : 5
-        },
-        {
-            id :2,
-            name : "CSS",
-            price : 199,
-            image : css,
-            rating : 5
-        },
-        {
-            id :3,
-            name :"JavaScripts",
-            price :499,
-            image : js,
-            rating : 4,
-        },
-        {
-            id :4,
-            name : "React",
-            price : 999,
-            image : react,
-            rating : 5
-        }
-    ]);
+    const [courses, setcourse] = useState(null);
 
     const [dummy, setdummy] = useState(true);
 
-    useEffect(() => {
-        console.log("useEffect Called")
-        console.log(dummy)
-    },[]);
+    useEffect(() =>{
+        fetch('http://localhost:3000/courses')
+        .then(response => {
+            console.log(response)
+            return response.json()
+        }).then(data => setcourse(data))
+    },[])
 
 
     function handleDelete(id){
@@ -52,12 +22,13 @@ function CourseList(){
         setcourse(newCourse)
     };
 
-    courses.sort((x,y) => y.price - x.price)
-    // courses.sort((p,g) => p.rating - g.rating)
+    //courses.sort((x,y) => y.price - x.price)
 
     // const vfmCourses = courses.filter((course) => course.price<200)
-    // const rcourses = courses.filter((courses) =>courses.rating>4 )
     
+    if (!courses){
+        return <></>
+    }
 
     const coursesList = courses.map(
         (course) => 
